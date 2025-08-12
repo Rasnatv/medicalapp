3611357bdccb91898b75504324ec1581e1fa2226
AbstractProgrammer@example.com <AbstractProgrammer@example.com>
2025-08-12 12:37:32 +0530

    shadowchanges2

--- lib/app/common/constants/shadow.dart
+++ lib/app/common/constants/shadow.dart
@@ -9,7 +9,7 @@ import 'package:innsouls_flutter/app/common/style/app_colors.dart';
 class Dshadowvertical{
 
   static final verticalproductShadow=BoxShadow(
-    color:Colors.grey.withOpacity(0.1),
+    color:Colors.grey.withOpacity(0.2),
     blurRadius: 50,
     spreadRadius: 7,
     offset: Offset(0,2)
